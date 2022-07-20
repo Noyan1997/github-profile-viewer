e257ef91e3ca18e683401d5694ce01b9341e4ac6
noyan ghahari <noyanghahari@gmail.com>
2022-07-21 02:14:03 +0430

    Create interface for user data in global.ts

---
+++ interface/global.ts
@@ -0,0 +1,11 @@
+export interface IUserData {
+  name: string
+  login: string
+  avatar_url: string
+  followers: string
+  following: string
+  bio: string
+  location: string
+  blog: string
+  message: string
+}

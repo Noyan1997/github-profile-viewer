5635a52915ca3e345af2cfa80e5b9343d2e1d142
noyan ghahari <noyanghahari@gmail.com>
2022-07-20 23:12:19 +0430

    Checking if the user is already logged in or not in username.txt

--- pages/profile/[username]/Username.tsx
+++ pages/profile/[username]/Username.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
-const UserName: NextPage = () => {
-  return <span>UserName</span>
+const UserName: NextPage = (props: any) => {
+  const { query, push } = useRouter()
+  if (props.users.message) {
+    // window.alert('nadari')
+    push('/')
+    return null
+  }
+  return <span>{`UserName ${query.username}`}</span>
 }
 
 export default UserName

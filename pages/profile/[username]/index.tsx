5b8cd3efc93af4ae834efe97a906fe21eb4168b9
noyan ghahari <noyanghahari@gmail.com>
2022-07-20 23:08:08 +0430

    send request to server and receive user's data

--- pages/profile/[username]/index.tsx
+++ pages/profile/[username]/index.tsx
@@ -1,1 +1,11 @@
 export { default } from './Username'
+
+export const getServerSideProps = async (context: any) => {
+  const _userName = context.query.username
+  const res = await fetch(`https://api.github.com/users/${_userName}`)
+  const users = await res.json()
+  //   console.log(2222, context)
+  return {
+    props: { users },
+  }
+}

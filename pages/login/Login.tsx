a6b72bde8dd913fb4018cd1a3d15f4965a6a877d
noyan ghahari <noyanghahari@gmail.com>
2022-07-20 13:19:11 +0430

    getting the value of user name in login.tsx

--- pages/login/Login.tsx
+++ pages/login/Login.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
 const Login: NextPage = () => {
-  return <span>Login</span>
+  const [value, setValue] = useState<string>()
+  return (
+    <>
+      <div>
+        <input
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          type="text"
+          placeholder="User name"
+        />
+        <input disabled type="password" placeholder="password" />
+        <button disabled={!value} onClick={() => console.log(value)}>
+          Sign in
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default Login

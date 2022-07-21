7b5fded3771641853dd2a69cca47e5e38f516176
noyan ghahari <noyanghahari@gmail.com>
2022-07-21 11:50:17 +0430

    putting up login component in fragment in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next'
 import Login from './login'
 
 const Home: NextPage = () => {
-  return <Login />
+  return (
+    <>
+      <Login />
+    </>
+  )
 }
 
 export default Home

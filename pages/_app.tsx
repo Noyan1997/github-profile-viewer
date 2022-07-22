aeabe6d7e1ac9ca5bb0dbc6e7eecedb9ac809a98
noyan ghahari <noyanghahari@gmail.com>
2022-07-22 18:58:00 +0430

    intel provider for multiple language in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { IntlProvider } from 'react-intl'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-      <ToastContainer limit={1} hideProgressBar={true} />
-    </ChakraProvider>
+    <>
+      <IntlProvider defaultLocale="en" locale="en">
+        <ChakraProvider>
+          <Component {...pageProps} />
+          <ToastContainer limit={1} hideProgressBar={true} />
+        </ChakraProvider>
+      </IntlProvider>
+    </>
   )
 }
 

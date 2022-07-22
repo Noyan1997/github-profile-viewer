c96ee9c616d2e35b6a38892b9a1365afc56608d6
noyan ghahari <noyanghahari@gmail.com>
2022-07-22 16:57:11 +0430

    improve error toast notification in Username.biz.ts

--- domains/Profile/Username/Username.biz.ts
+++ domains/Profile/Username/Username.biz.ts
@@ -1,14 +1,21 @@
+import { useToast } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useEffect, useRef } from 'react'
-import { toast } from 'react-toastify'
 import { IUserData } from '../../../interface/global'
 const useUsername = (users: IUserData) => {
   const { push } = useRouter()
   const isMountedRef = useRef<boolean>(false)
-
+  const toast = useToast()
   useEffect(() => {
     if (users.message && !isMountedRef.current) {
-      toast.error('There is no user with this username')
+      //   toast.error('There is no user with this username')
+      toast({
+        title: 'User not found!',
+        description: 'Check username and try again',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
       // prevent to extra routing and rerendering
       isMountedRef.current = true
       push('/')

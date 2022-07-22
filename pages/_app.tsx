8e8bda3b0cfa6b168087081e41a5315d48162b08
noyan ghahari <noyanghahari@gmail.com>
2022-07-22 12:12:15 +0430

    refactor-migrate style files a to appropriate directori

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import '../styles/globals.css'
-import '../styles/userInformation.scss'
-import '../styles/userName.scss'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/login/Login.tsx
+++ pages/login/Login.tsx
@@ -1,3 +1,6 @@
+import { Box } from '@chakra-ui/layout'
+import { Input } from '@chakra-ui/react'
+import GitHubIcon from '@mui/icons-material/GitHub'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
@@ -8,17 +11,21 @@ const Login: NextPage = () => {
 
   return (
     <>
-      <div>
-        <input
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
-          type="text"
-          placeholder="User name"
-        />
-        <input disabled type="password" placeholder="password" />
-        <button disabled={!value} onClick={() => push(`/profile/${value}`)}>
-          Sign in
-        </button>
+      <div className="login_page_parrent column w-100 j-center a-center ">
+        <GitHubIcon />
+        <span>Sign in to GitHub</span>
+        <Box className="column">
+          <Input
+            placeholder="User name"
+            type="text"
+            onChange={(e: any) => setValue(e.target.value)}
+            value={value}
+          />
+          <Input placeholder="password" disabled />
+          <button disabled={!value} onClick={() => push(`/profile/${value}`)}>
+            Sign in
+          </button>
+        </Box>
       </div>
     </>
   )
